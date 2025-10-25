a1c83031ac3a0e52976d6f2449460a7742389217
dachishengelia <shengeliaqeti111@gmail.com>
2025-10-25 15:13:27 +0400

    first commit

---
+++ middlewares/isAuth.middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const isAuth = async (req, res, next) => {
+    const headers = req.headers['authorization']
+    if(!headers) {
+        return res.status(401).json({message: "you dont have permition"})
+    }
+
+    const [type, token] = headers.split(' ')
+    try{
+        const payload = await jwt.verify(token, process.env.JWT_SECRET)
+        req.userId = payload.userIdc
+        req.role = payload.role
+
+        next()
+    }catch(e){
+        return res.status(401).json({message: "you dont have permition"})
+    }
+}
+
+module.exports = isAuth
+
