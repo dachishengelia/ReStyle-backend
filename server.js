54a3ec9decfd7d1f660939ccffd3c2f7ac98f998
dachishengelia <shengeliaqeti111@gmail.com>
2025-11-14 21:57:09 +0400

    products

--- server.js
+++ server.js
@@ -15,18 +15,7 @@ const app = express();
 
 const allowedOrigins = [process.env.FRONTEND_URL, "https://re-style-frontend.vercel.app/"];
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cookieParser());
