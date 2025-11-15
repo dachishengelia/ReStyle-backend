888b6cedb74cf72587baf20a012e9fcef7ec0e04
dachishengelia <shengeliaqeti111@gmail.com>
2025-11-15 14:50:25 +0400

    cloudinary

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -5,14 +5,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-cloudinary.v2.config({
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary.v2,
+  cloudinary: cloudinary,
   params: {
     folder: "uploads",
     allowed_formats: ["jpg", "png", "svg", "jpeg"],
@@ -23,12 +23,11 @@ const upload = multer({ storage });
 
 const deleteFromCloudinary = async (publicId) => {
   try {
-    const result = await cloudinary.v2.uploader.destroy(publicId);
+    const result = await cloudinary.uploader.destroy(publicId);
     console.log("Cloudinary delete result:", result);
   } catch (error) {
     console.error("Error deleting from Cloudinary:", error);
   }
 };
 
-// Use ES module exports
 export { upload, deleteFromCloudinary };
