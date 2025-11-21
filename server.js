2fea1c7324e3eb8ff4ca2ee2ce62244b28e144bc
dachishengelia <shengeliaqeti111@gmail.com>
2025-11-21 20:14:59 +0400

    test

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ import productRoutes from "./routes/Product.js";
 
 const app = express();
 
-const allowedOrigins = [process.env.FRONTEND_URL, process.env.FRONTEND_VERCEL_URL];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://re-style-frontend.vercel.app",
+  "https://re-style-frontend-mqiajixrh-dachi-shengelias-projects.vercel.app", // Add this origin
+];
 
 app.use(
   cors({
