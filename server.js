04935f25ea0106bcef024a3bfd6a2b504fdadcce
dachishengelia <shengeliaqeti111@gmail.com>
2025-11-29 14:52:10 +0400

    fix vercel

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ app.use(
         callback(new Error("CORS blocked: " + origin));
       }
     },
-    // credentials: true, 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], 
     allowedHeaders: ["Content-Type", "Authorization"], 
   })
