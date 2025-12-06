8942b83ff1e69b3bfb9c1d83ac248b7f13817b9f
dachishengelia <shengeliaqeti111@gmail.com>
2025-12-06 15:05:24 +0400

    commitz'

--- server.js
+++ server.js
@@ -17,33 +17,6 @@ import productRoutes from "./routes/Product.js";
 import connectToDb from "./db/connectToDB.js";
 
 
-
-
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "https://re-style-frontend.vercel.app",
-// ];
-
-
-// app.use(cors({
-//   origin: function(origin, callback) {
-
-//     if (!origin) return callback(null, true);
-
-//     if (allowedOrigins.includes(origin)) {
-//       return callback(null, true);
-//     }
-
-//     return callback(new Error("Not allowed by CORS"));
-//   },
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
-// }));
-
-
-// app.options("*", cors());
-
 app.use(cors({origin: [process.env.FRONTEND_URL, process.env.FRONTEND_VERCEL_URL], credentials: true}));
 
 
